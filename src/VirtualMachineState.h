5763c365e1b4c892c57f3cc49c78380c30db2e08
Steven Piantadosi <spiantado@gmail.com>
2018-12-06 13:16:52 -0800

    added constexpr so that we don't need to include all types for builtin ops (e..g bool, short, double, etc.).

--- src/VirtualMachineState.h
+++ src/VirtualMachineState.h
@@ -1,27 +1,37 @@
 #ifndef VIRTUAL_MACHINE_STATE
 #define VIRTUAL_MACHINE_STATE
 
-// NOTE: CURRNTLY ERR IS NOT HANDLED RIGHT -- SHOULD HAVE A STACK I THINK?
-
-
-
+#include <type_traits>
 /* 
  * 
  * We should rewrite with an "index" stack, and an index type (e.g. short). 
  * That would prevent us from having to use "short"
  * and it will make memoization easier -- make mem a tuple
  * 
- * We can probably avoid doing nt_bool if we use an IF statement to check if we have
+ * We can probably avoid doing nt_bool if we use an #ifdef statement to check if we have
  * defined op_FLIP?
  * 
+ * NOTE: CURRNTLY ERR IS NOT HANDLED RIGHT -- SHOULD HAVE A STACK I THINK?
  */
-
 
 unsigned long global_vm_ops = 0;
 
 // Remove n from the stack
-void popn(Opstack& s, size_t n) {
+void popn(Program& s, size_t n) {
 	for(size_t i=0;i<n;i++) s.pop();
+}
+
+
+
+template<typename X, typename t>
+constexpr bool contains_type() { return std::is_same<X,t>::value; }
+template<typename X, typename t, typename... T>
+constexpr bool contains_type() {
+	// check if my template args contian a given type
+	if constexpr (std::is_same<X, t>::value) {  //|| contains_type<X,T...> 
+		return true;
+	}
+	else return false;
 }
 
 
@@ -36,7 +46,7 @@ public:
 	static const unsigned long MAX_RECURSE = 64;
 	static constexpr double    LP_BREAKOUT = 5.0; // we keep executing a probabilistic thread as long as it doesn't cost us more than this compared to the top
 	
-	Opstack            opstack; 
+	Program            opstack; 
 	std::stack<t_x>    xstack; //xstackthis stores a stack of the x values (for recursive calls)
 	t_return           err; // what error output do we return?
 	double             lp; // the probability of this context
@@ -118,233 +128,162 @@ public:
 		while(!opstack.empty()){
 			if(aborted) return err;
 			
-			op_t op = opstack.top(); opstack.pop();
-			++global_vm_ops;
-
-			//CERR "OP = " << op ENDL;
+			Instruction i = opstack.top(); opstack.pop();
 			
-			switch(op) {
-				case op_NOP: 
-				{
-					break;
-				}
-				case op_X:
-				{
-					push<t_x>(xstack.top());
-					break;
-				}
-				case op_POPX:
-				{
-					xstack.pop(); // NOTE: Remember NOT to pop from getpop<t_x>() since that's not where x is stored
-					break;
-				}
-				case op_MEM:
-				{
-					// by definition, this peeks at the top of t_x and t_return and caches
-//					mem[xstack.top()] = std::get<std::stack<t_return>>(stack.value).top();
-					
-				}
-				case op_RECURSE:
-				{
-					if(recursion_depth++ > MAX_RECURSE) { // there is one of these for each recurse
-						aborted = RECURSION_DEPTH;
-						return err;
-					}
-					
-					// if we get here, then we have processed our arguments and they are stored in the t_x stack. 
-					// so we must move them to the x stack (where there are accessible by op_X)
-					xstack.push(getpop<t_x>());
-					opstack.push(op_POPX); // we have to remember to remove X once the other program evaluates, *after* everything has evaluated
-					
-					loader->push_program(opstack,0); // push this program 
-					
-					// after execution is done, the result will be pushed onto t_return
-					// which is what gets returned when we are all done
-					
-					break;
-				}
-				case op_RECURSE_FACTORIZED:
-				{
-					if(recursion_depth++ > MAX_RECURSE) {
-						aborted = RECURSION_DEPTH;
-						return err;
-					}
-					short idx = getpop<short>(); // shorts are *always* used to index into factorized lexica
-					
-					xstack.push(getpop<t_x>());							
-					opstack.push(op_POPX); 
-					
-					loader->push_program(opstack,idx);
-					
-					break;
-				}
-//				case op_MEM_RECURSE:
-//				{
-//					if(recursion_depth++ > MAX_RECURSE) { // there is one of these for each recurse
-//						aborted = RECURSION_DEPTH;
-//						return err;
-//					}
-//					
-//					// if we get here, then we have processed our arguments and they are stored in the t_x stack. 
-//					// so we must move them to the x stack (where there are accessible by op_X)
-//					auto m = std::make_pair(0, getpop<t_x>());
-//					
-//					if(mem.count(m)) {  // if we have done this already
-//						push<t_return>(mem[m]); // return this value
-//					}
-//					else { // we have to evaluate 
-// 						
-//						xstack.push(getpop<t_x>());
-//
-//						opstack.push(op_POPX); // we have to remember to remove X once the other program evaluates, *after* everything has evaluated
-//						opstack.push(op_MEM); // mem looks at the top of the stack and caches the current x value
-//						push<short>(0); // we used index zero since non-factorized 
-//						loader->push_program(opstack,0); // push this program 
-//					}
-//					
-//					break;
-//				}
-//				case op_MEM_RECURSE_FACTORIZED:
-//				{
-//					if(recursion_depth++ > MAX_RECURSE) {
-//						aborted = RECURSION_DEPTH;
-//						return err;
-//					}
-//					short idx = getpop<short>(); // shorts are *always* used to index into factorized lexica
-//					
-//					auto m = std::make_pair(0, getpop<t_x>());
-//					
-//					if(mem.count(m)) {  // if we have done this already
-//						push<t_return>(mem[m]); // return this value
-//					}
-//					else {
-//					
-//						xstack.push(getpop<t_x>());							
-//						opstack.push(op_POPX); 
-//						
-//						loader->push_program(opstack,idx);
-//						continue;
-//					}
-//					
-//					break;
-//				}
-//				case op_MEM_RECURSE_FACTORIZED:
-//				{
-//					if(recursion_depth++ > MAX_RECURSE) {
-//						aborted = RECURSION_DEPTH;
-//						return err;
-//					}
-//					short idx = getpop<short>(); // shorts are *always* used to index into factorized lexica
-//					
-//					xstack.push(getpop<t_x>());							
-//					opstack.push(op_POPX); 
-//					
-//					loader->push_program(opstack,idx);
-//					continue;
-//
-//					break;
-//				}
-//				case op_MEM: 
-//				{
-//					// need to get x and return value off stack
-//					break;
-//				}
-				case op_FLIP: 
-				{
-					// flip with default 0.5 arg
-					push<double>(0.5);
-					
-					// and fall through
-				}
-				//[[fallthrough]];  // hmm I thought I needed this, to mark fallthroughs, but it gives me a warning!
-				case op_FLIPP:
-				{
-					
-					
-					assert(pool != nullptr && "op_FLIP and op_FLIPP require the pool to be non-null, since they push onto the pool"); // can't do that, for sure
-				
-					double p = getpop<double>(); // reads a double argfor the coin weight
-					if(std::isnan(p)) { p = 0.0; } // treat nans as 0s
-					assert(p <= 1.0 && p >= 0.0);
-					
-					// In this implementation, we keep going as long as the lp doesn't drop too
-					// low, always assuming if evaluates to true
-					
-					VirtualMachineState<t_x,t_return>* v0 = new VirtualMachineState<t_x,t_return>(*this);
-					v0->increment_lp(log(1.0-p));
-					v0->push<bool>(false); // add this value to the stack since we make this choice
-					pool->push(v0);					
-					
-					
-					VirtualMachineState<t_x,t_return>* v1 = new VirtualMachineState<t_x,t_return>(*this);
-					v1->increment_lp(log(p));
-					v1->push<bool>(true); // add this value to the stack since we make this choice
-					pool->push(v1);	
-					
-					aborted = RANDOM_CHOICE; // this context is aborted please 
+			++global_vm_ops;
+			
+			if(i.is_custom) {
+				t_abort b = dispatch->dispatch_rule(i, pool, this, loader);
+				if(b != NO_ABORT) {
+					aborted = b;
 					return err;
-					/*
-					// and then how we follow the true branch
-					increment_lp(log(p));
-					push<bool>(true); // add this value to the stack since we make this choice
+				}
+			}
+			else {
+				
+				switch(i.builtin) {
+					case op_NOP: 
+					{
+						break;
+					}
+					case op_X:
+					{
+						push<t_x>(xstack.top());
+						break;
+					}
+					case op_POPX:
+					{
+						xstack.pop(); // NOTE: Remember NOT to pop from getpop<t_x>() since that's not where x is stored
+						break;
+					}
+					case op_RECURSE:
+					{
+						if(recursion_depth++ > MAX_RECURSE) { // there is one of these for each recurse
+							aborted = RECURSION_DEPTH;
+							return err;
+						}
 						
-					// now if we can't just keep going, copy myself and put me on the stack
-					if(lp < pool->Q.top()->lp - LP_BREAKOUT) { // TODO: INCLUDE THE CONTEXT LIMIT HERE 
-						pool->push(new VirtualMachineState<t_x,t_return>(*this));
+						// if we get here, then we have processed our arguments and they are stored in the t_x stack. 
+						// so we must move them to the x stack (where there are accessible by op_X)
+						xstack.push(getpop<t_x>());
+						opstack.push(Instruction(op_POPX)); // we have to remember to remove X once the other program evaluates, *after* everything has evaluated
+						
+						loader->push_program(opstack,0); // push this program 
+						
+						// after execution is done, the result will be pushed onto t_return
+						// which is what gets returned when we are all done
+						
+						break;
+					}
+					case op_RECURSE_FACTORIZED:
+					{
+						// the index of who we recurse to is assumed stored in arg
+						if(recursion_depth++ > MAX_RECURSE) {
+							aborted = RECURSION_DEPTH;
+							return err;
+						}
+						
+						xstack.push(getpop<t_x>());							
+						opstack.push(Instruction(op_POPX));
+						
+						loader->push_program(opstack,i.arg);
+						
+						break;
+					}
+					case op_FLIP: 
+					{
+						// We're going to duplicate code a little bit so that we 
+						// don't need to have double defined for FLIP (e.g. p=0.5 always)
+						if constexpr (contains_type<bool,NT_TYPES>()) { 
+						
+							double p = 0.5; 
+
+							VirtualMachineState<t_x,t_return>* v0 = new VirtualMachineState<t_x,t_return>(*this);
+							v0->increment_lp(log(1.0-p));
+							v0->push<bool>(false); // add this value to the stack since we make this choice
+							pool->push(v0);					
+													
+							VirtualMachineState<t_x,t_return>* v1 = new VirtualMachineState<t_x,t_return>(*this);
+							v1->increment_lp(log(p));
+							v1->push<bool>(true); // add this value to the stack since we make this choice
+							pool->push(v1);	
+							
+							aborted = RANDOM_CHOICE; // this context is aborted please 
+							return err;
+		
+						// and fall through
+						} else { assert(0 && "*** Cannot use op_FLIP without defining bool in NT_TYPES"); }
+					}
+					case op_FLIPP:
+					{
+						if constexpr (contains_type<bool,NT_TYPES>() && contains_type<double,NT_TYPES>() ) { 
+						assert(pool != nullptr && "op_FLIP and op_FLIPP require the pool to be non-null, since they push onto the pool"); // can't do that, for sure
+					
+						double p = getpop<double>(); // reads a double argfor the coin weight
+						if(std::isnan(p)) { p = 0.0; } // treat nans as 0s
+						assert(p <= 1.0 && p >= 0.0);
+						
+						VirtualMachineState<t_x,t_return>* v0 = new VirtualMachineState<t_x,t_return>(*this);
+						v0->increment_lp(log(1.0-p));
+						v0->push<bool>(false); // add this value to the stack since we make this choice
+						pool->push(v0);					
+												
+						VirtualMachineState<t_x,t_return>* v1 = new VirtualMachineState<t_x,t_return>(*this);
+						v1->increment_lp(log(p));
+						v1->push<bool>(true); // add this value to the stack since we make this choice
+						pool->push(v1);	
 						
 						aborted = RANDOM_CHOICE; // this context is aborted please 
+						return err;
+						/*
 						
-						return err;
-					}
-					// else continue -- keep going now that true is pushed
-					*/
+						// In this implementation, we keep going as long as the lp doesn't drop too
+						// low, always assuming if evaluates to true
+						// and then how we follow the true branch
+						increment_lp(log(p));
+						push<bool>(true); // add this value to the stack since we make this choice
+							
+						// now if we can't just keep going, copy myself and put me on the stack
+						if(lp < pool->Q.top()->lp - LP_BREAKOUT) { // TODO: INCLUDE THE CONTEXT LIMIT HERE 
+							pool->push(new VirtualMachineState<t_x,t_return>(*this));
+							
+							aborted = RANDOM_CHOICE; // this context is aborted please 
+							
+							return err;
+						}
+						// else continue -- keep going now that true is pushed
+						*/
 
-					break;
-					
-				}
-				case op_JMP:
-				{
-					// jump relative by taking the next item off the instruction pointer and treating it as an integer offset
-					// NOTE: This is not the smartest thing to do since it requires storing an integer offset as a op code, but
-					// it should be fine
-					// NOTE: We have not implemented negative jumps yet -- because we are using a stack. 
-					// but that's okay because all that is needed is positive to implement IF
-					
-					int r = (int)opstack.top(); // not safe or wonderful but should be okay to use ops to encode jump sizes
-					opstack.pop(); // because we just removed (then cast) one for r
-					
-					// now remove as many as r told you to
-					popn(opstack, r);
-					
-					break;
-				}
-				case op_IF: 
-				{
-					// Here we evaluate op_IF, which has to short circuit and skip (pop some of the stack) 
-					bool b = getpop<bool>(); // bool has already evaluted
-					
-					int xsize = (int) opstack.top(); opstack.pop();
-					
-					if(!b) {  // now ops must skip the xbranch
-						popn(opstack, xsize);
+						break;
+						} else { assert(0 && "*** Cannot use op_FLIPP without defining bool and double in NT_TYPES"); }
 					}
-					else {
-						// do nothing, we pass through and then get to the jump we placed at the end of the x branch
+
+					case op_JMP:
+					{
+						popn(opstack, i.arg);
+						break;
 					}
-					
-					break;					
-				}
-				default: 
-				{
-					// otherwise call my dispatch and return if there is an error 
-					t_abort b = dispatch->dispatch_rule(op, pool, this, loader);
-					if(b != NO_ABORT) {
-						aborted = b;
-						return err;
+					case op_IF: 
+					{
+						if constexpr (contains_type<bool,NT_TYPES>()) { 
+						// Here we evaluate op_IF, which has to short circuit and skip (pop some of the stack) 
+						bool b = getpop<bool>(); // bool has already evaluted
+						
+						// now ops must skip the xbranch
+						if(!b) popn(opstack, i.arg);
+						else   {}; // do nothing, we pass through and then get to the jump we placed at the end of the x branch
+						
+						break;		
+						} else { assert(0 && "*** Cannot use op_IF without defining bool in NT_TYPES"); }			
 					}
-				}
-			} // end switch
+					default: 
+					{
+						// otherwise call my dispatch and return if there is an error 
+						assert(0 && "Bad op name");
+					}
+				} // end switch
 			
+			} // end if not custom
 		} // end loop over ops
 	
 		if(aborted) return err;		
