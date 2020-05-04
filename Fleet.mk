b3bcc7cfe313c7f66a049547bf4e45f3d0842e24
Steven Piantadosi <spiantado@gmail.com>
2020-05-03 23:22:10 -0700

    updated Makefile to include LIBS

--- Fleet.mk
+++ Fleet.mk
@@ -2,8 +2,9 @@
 FLEET_INCLUDE=-I../../src/ -I../../src/Inference -I../../src/Hypotheses -I../../src/VirtualMachine -I../../src/Statistics -I../../src/Grammar
 
 # Some standard/default flags
-FLEET_FLAGS=-std=c++2a -Wall -fdiagnostics-color=auto -Wimplicit-fallthrough -Wall -Wextra -Wextra-semi -Wpedantic -Wvla -Wnull-dereference -Wswitch-enum -Wno-unused-parameter -fvar-tracking-assignments -Wduplicated-cond -Wduplicated-branches -Wsuggest-override -pthread -march=native -ftemplate-backtrace-limit=0 -fstack-protector-strong --param max-inline-insns-recursive=100000
+FLEET_FLAGS=-std=c++2a -Wall -fdiagnostics-color=auto -Wimplicit-fallthrough -Wall -Wextra -Wextra-semi -Wpedantic -Wvla -Wnull-dereference -Wswitch-enum -Wno-unused-parameter -fvar-tracking-assignments -Wduplicated-cond -Wduplicated-branches -Wsuggest-override -march=native -ftemplate-backtrace-limit=0 -fstack-protector-strong --param max-inline-insns-recursive=100000
+## Might add -Werror ?
 
-## Might add -Werror 
+FLEET_LIBS=-lm -pthread
 
-CLANG_FLAGS=-std=c++2a -Wall -fdiagnostics-color=auto -Wimplicit-fallthrough -Wall -Wextra -Wextra-semi -Wpedantic -Wvla -Wnull-dereference -Wswitch-enum -Wno-unused-parameter -pthread -march=native 
+CLANG_FLAGS=-std=c++2a -Wall -fdiagnostics-color=auto -Wimplicit-fallthrough -Wall -Wextra -Wextra-semi -Wpedantic -Wvla -Wnull-dereference -Wswitch-enum -Wno-unused-parameter  -march=native 
