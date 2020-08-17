7d24023adce05dd1a526c89c4a1e61758ca1a156
Steven Piantadosi <spiantado@gmail.com>
2020-08-16 22:53:27 -0700

    rewrite of NumberGame

--- Models/NumberGame/Main.cpp
+++ Models/NumberGame/Main.cpp
@@ -7,22 +7,136 @@
 
 const double reliability = 0.99;
 const int N = 100; // what number do we go up to?
+const int m = 1; // min -- 1 or 0, who can ever remember?
 
 ///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-/// Define magic primitives
+/// Define primitives
 ///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#include <set>
+
+/**
+ * @class ST
+ * @author Steven Piantadosi
+ * @date 16/08/20
+ * @file Main.cpp
+ * @brief We define a version of std::set that caps out at 100 (although you're not really supposed to inherit from std containers)
+ */
+class ST : public std::set<int>{
+public:
+	ST() {}
+	
+	ST(std::initializer_list<int> l) : std::set<int>(l) {
+	}
+
+	void insert(int x) {
+		if(x <= N and x >= m){
+			std::set<int>::insert(x);
+		}
+		else {
+			// nothing
+		}
+	}
+};
+
+// first we're going to declare some sets that are relevant
+const ST numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
+const ST primes  = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+const ST evens   = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100};
+const ST odds    = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99};
+const ST squares = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
+const ST decades = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
 
 #include "Primitives.h"
 #include "Builtins.h"
 
+
 std::tuple PRIMITIVES = {
-	Primitive("(%s+%s)",    +[](float a, float b) -> float { return a+b; }),
-	Primitive("(%s-%s)",    +[](float a, float b) -> float { return a-b; }),
-	Primitive("(%s*%s)",    +[](float a, float b) -> float { return a*b; }),
-	Primitive("(%s/%s)",    +[](float a, float b) -> float { return (b==0 ? 0 : a/b); }),
-	Primitive("(%s^%s)",    +[](float a, float b) -> float { return pow(a,b); }),
 	
-	Builtin::X<float>("x")
+	
+	Primitive("numbers",    +[]() -> ST { return numbers; }),
+	Primitive("primes",     +[]() -> ST { return primes; }),
+	Primitive("evens",      +[]() -> ST { return evens; }),
+	Primitive("odds",       +[]() -> ST { return odds; }),
+	Primitive("squares",    +[]() -> ST { return squares; }),
+	Primitive("decades",    +[]() -> ST { return decades; }),
+	
+	// we give range a very low prior here or else it sure dominates
+	Primitive("range(%s,%s)",  +[](int x, int y) -> ST { 
+		ST out;
+		for(int i=std::max(x,m);i<=std::min(y,N);i++) { // we need min/max bounds here or we spend all our time adding garbage
+			out.insert(i);
+		}
+		return out;
+	}, 0.1),
+	
+	// ADD UNION, INTERSECTION, MIN, MAX
+	
+	Primitive("union(%s,%s)",    +[](ST& a, ST b) -> void { 
+		for(auto& x : b) {
+			a.insert(x);
+		}
+	}),
+	
+	Primitive("intersection(%s,%s)",    +[](ST a, ST b) -> ST { 
+		ST out;
+		for(auto& x : a) {
+			if(b.find(x) != b.end())
+				out.insert(x);
+		}
+		return out;
+	}),
+
+
+	Primitive("complement(%s)",    +[](ST a) -> ST { 
+		ST out;
+		for(size_t i=m;i<N;i++) {
+			if(a.find(i) == a.end())
+				out.insert(i);
+		}
+		return out;
+	}),
+
+	
+	Primitive("(%s+%s)",    +[](ST s, int n) -> ST { 
+		ST out;
+		for(auto& x : s) {
+			out.insert(x + n);
+		}
+		return out;
+	}),
+	
+	Primitive("(%s*%s)",    +[](ST s, int n) -> ST { 
+		ST out;
+		for(auto& x : s) {
+			out.insert(x * n);
+		}
+		return out;
+	}),
+	
+	Primitive("(%s^%s)",    +[](ST s, int n) -> ST { 
+		ST out;
+		for(auto& x : s) {
+			out.insert(std::pow(x,n));
+		}
+		return out;
+	}),	
+	Primitive("(%s^%s)",    +[](int n, ST s) -> ST { 
+		ST out;
+		for(auto& x : s) {
+			out.insert(std::pow(n,x));
+		}
+		return out;
+	}),
+	
+	// int operations
+	Primitive("(%s+%s)",    +[](int x, int y) -> int { return x+y;}),
+	Primitive("(%s*%s)",    +[](int x, int y) -> int { return x*y;}),
+	Primitive("(%s-%s)",    +[](int x, int y) -> int { return x-y;}),
+	Primitive("(%s^%s)",    +[](int x, int y) -> int { return std::pow(x,y);}),
+	
+	Builtin::X<int>("x", 1.0)
 };
 
 
@@ -32,8 +146,8 @@ std::tuple PRIMITIVES = {
 
 #include "Grammar.h"
 
-class MyGrammar : public Grammar<float> {
-	using Super=Grammar<float>;
+class MyGrammar : public Grammar<ST,int> {
+	using Super=Grammar<ST,int>;
 	using Super::Super;
 };
 
@@ -46,30 +160,21 @@ class MyGrammar : public Grammar<float> {
 #include<set>
 #include "LOTHypothesis.h"
 
-class MyHypothesis final : public LOTHypothesis<MyHypothesis,float,float,MyGrammar,float,std::multiset<float> > {
+class MyHypothesis final : public LOTHypothesis<MyHypothesis,int,ST,MyGrammar,int,std::multiset<int> > {
 public:
-	using Super = LOTHypothesis<MyHypothesis,float,float,MyGrammar,float,std::multiset<float>>;
+	using Super = LOTHypothesis<MyHypothesis,int,ST,MyGrammar,int,std::multiset<int> >;
 	using Super::Super; // inherit the constructors
 	
 	virtual double compute_likelihood(const data_t& data, const double breakout=-infinity) override {
 		// TODO: This uses 0 for NAN -- probably not the best...
 		
-		std::set<float> s;
-		float fx;
-		for(int i=0;i<=N and !CTRL_C;i++) {
-			fx = this->callOne(i, 0, this);
-			
-			if(fx >=0 and fx <= N and !std::isnan(fx)) { // important to check bounds for size principle
-				s.insert(fx);
-			}
-		}
-		
-		double sz = s.size(); // size principle likelihood
+		ST out = callOne(1); // just give x=1
+		double sz = out.size(); // size principle likelihood
 		
 		// now go through and compute the likelihood
 		likelihood = 0.0;
 		for(auto& d : data) {
-			bool b = (s.find(d)!=s.end()); // does it contain?
+			bool b = (out.find(d)!=out.end()); // does it contain?
 			likelihood += log(  (b ? reliability / sz : 0.0) + (1.0-reliability)/N);
 		}
 		
@@ -87,15 +192,14 @@ public:
 #include "Top.h"
 #include "MCMCChain.h"
 
-int main(int argc, char** argv){ 
-	auto app = Fleet::DefaultArguments("Number game");
-	CLI11_PARSE(app, argc, argv);
-	Fleet_initialize();
+int main(int argc, char** argv){ 	
+	Fleet fleet("Number Game");
+	fleet.initialize(argc, argv);
 
 	MyGrammar grammar(PRIMITIVES);
 	
-	for(int i=0;i<=N;i++) {
-		grammar.add<float>(BuiltinOp::op_FLOAT, str(i), 10.0/N, i);		
+	for(int i=m;i<=N;i++) {
+		grammar.add<int>(BuiltinOp::op_INT, str(i), 10.0/N, i);		
 	}
 
 
@@ -103,7 +207,9 @@ int main(int argc, char** argv){
 	TopN<MyHypothesis> top(ntop);
 
 	// our data
-	MyHypothesis::data_t mydata = {2,8,16};	
+	//MyHypothesis::data_t mydata = {25, 36, 49, 25, 36, 49, 25, 36, 49, 25, 36, 49};	// squares in a range
+	//MyHypothesis::data_t mydata = {3,4,6,8,12};	
+	MyHypothesis::data_t mydata = {2,4,32};	
 
 	// create a hypothesis
 	auto h0 = MyHypothesis::make(&grammar);
