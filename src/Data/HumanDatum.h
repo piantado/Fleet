1d78c8e6e7c83a1742838db05f17dbe94be52b0f
Steven Piantadosi <spiantado@gmail.com>
2020-09-07 11:05:34 -0700

    fixed type in HumanData

--- src/Data/HumanDatum.h
+++ src/Data/HumanDatum.h
@@ -13,13 +13,13 @@
  */
 template<typename HYP, typename _output_t=HYP::output_t, typename _datum_t=HYP::datum_t, typename _data_t=HYP::data_t>
 struct HumanDatum {
-	typedef _data_t  data_t;
-	typedef _datum_t datum_t;
+	typedef _data_t   data_t;
+	typedef _datum_t  datum_t;
 	typedef _output_t output_t;
 	
 	data_t*                   data; 
 	size_t                    ndata; // we condition on the first ndata points in data (so we can point to a single stored data)
-	datum_t*                  predict;	// what we compute on now
+	HYP::input_t*             predict;	// what we compute on now
 	std::map<output_t,size_t> responses; // how many of each type of response do you see?
 	double                    chance; // how many responses are alltogether possible? Needed for chance responding. 
 	int                       decay_position; // for memory decay that's decay_position**(-decay)
