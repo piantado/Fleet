8dcdb7e51adcc4189614f7c766636a0fd208b9a9
Steven Piantadosi <spiantado@gmail.com>
2020-12-31 11:04:04 -0800

    _

--- Testing/Inference/plot.R
+++ Testing/Inference/plot.R
@@ -1,15 +1,23 @@
 library(ggplot2)
 
-d <- read.table("o.txt", header=F)
-names(d) <- c("repetition", "runtime", "method", "restart", "data", "chains", "inner.times", "explore", "posterior", "prior", "likelihood", "hypothesis")
+d <- read.table("o2.txt", header=F)
+names(d) <- c("repetition", "runtime", "method", "restart", "data", "chains", "inner.times", "explore", "partition.depth", "posterior", "prior", "likelihood", "hypothesis")
+
+d <- subset(d, restart==0)
 
 d$chains <- as.factor(d$chains)
 d$restart <- as.factor(d$restart)
 
-plt <- ggplot(d, aes(x=runtime, y=posterior, color=chains, group=chains)) + 
+d$method.plot <- as.character(d$method)
+d$method.plot <- ifelse(d$method=="partition-mcmc",     paste0("partition-mcmc ", d$partition.depth),             d$method.plot)
+d$method.plot <- ifelse(d$method=="mcmc-within-mcts",   paste0("mcmc-within-mcts ", d$inner.times, d$explore),    d$method.plot)
+d$method.plot <- ifelse(d$method=="prior-sample-mcts",  paste0("prior-sample-mcts ", d$inner.times, d$explore),   d$method.plot)
+# d$method.plot <- ifelse(d$method=="parallel-tempering", paste0("parallel-tempering ", d$chains),                  d$method.plot)
+
+plt <- ggplot(subset(d, restart=0), aes(x=runtime, y=posterior, color=chains, group=chains)) + 
     stat_summary(fun=mean, geom="line") + 
     scale_x_log10() +
-    facet_grid(data~method, scales="free") + 
+    facet_grid(data ~ method.plot, scales="free") + 
     theme_bw()
 plt
 
