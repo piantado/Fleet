c5c289eec6f6ede45ec436525976271a147828eb
Steve Piantadosi <spiantado@gmail.com>
2021-01-02 23:03:38 -0800

    refactoring enumeration into classes (haven't updated testing or infernece yet)

--- src/Grammar/Enumeration.h
+++ src/Grammar/Enumeration.h
@@ -10,88 +10,141 @@
  * @file Enumeration.h
  * @brief This interface an easy enumeratino interface which provides an abstract class for various
  * 		  types of enumeration. Most need to know about the grammar type.  
+ * 
+ * 		  NOTE: This is NOT the infernece scheme -- for that see Inference/Enumeration.h
  */
 template<typename Grammar_t>
 class EnumerationInterface {
+public:
+	Grammar_t* grammar;
 
+	EnumerationInterface(Grammar_t* g) : grammar(g) {
 
+	}
+
+	/**
+	 * @brief Subclass must implement for IntegerizedStack
+	 * @param nt
+	 * @param is
+	 * @return 
+	 */
+	virtual Node toNode(nonterminal_t nt, IntegerizedStack& is)  {
+		throw NotImplementedError("*** Subclasses must implement toNode");
+	}
+	virtual Node toNode(nonterminal_t nt, const Node& frm, IntegerizedStack& is) {
+		throw NotImplementedError("*** Subclasses must implement toNode");
+	}
+	
+	// ensure these don't get called 
+	//virtual Node toNode(nonterminal_t nt, IntegerizedStack is) = delete;
+	//virtual Node toNode(nonterminal_t nt, const Node& frm, IntegerizedStack is) = delete; 
+	
+	/**
+	 * @brief Expand an integer to a node using this nonterminal type. 
+	 * @param nt
+	 * @param z
+	 * @return 
+	 */
+	virtual Node toNode(nonterminal_t nt, enumerationidx_t z) {
+		++FleetStatistics::enumeration_steps;
+	
+		IntegerizedStack is(z);
+		return toNode(nt, is);
+	}
+	
+	virtual Node toNode(nonterminal_t nt, const Node& frm, enumerationidx_t z) {
+		++FleetStatistics::enumeration_steps;
+	
+		IntegerizedStack is(z);
+		return toNode(nt, frm, is);
+	}
+	
+	/**
+	 * @brief Convert a node to an integer (should be the inverse of toNode)
+	 * @param n
+	 * @return 
+	 */
+	virtual enumerationidx_t toInteger(const Node& n) {
+		throw NotImplementedError("*** Subclasses must implement toInteger(Node&)");
+	}
+	
+	/**
+	 * @brief Convert a node to an integer where we come from something
+	 * @param frm
+	 * @param n
+	 * @return 
+	 */
+	virtual enumerationidx_t toInteger(const Node& frm, const Node& n) {
+		throw NotImplementedError("*** Subclasses must implement toInteger(Node&,Node&)");
+	}
+	
 	
 };
 
 
-/**
- * @brief This is a handy function to enumerate trees produced by the grammar. 
- *  This works by encoding each tree into an integer using some "pairing functions" 
- *  (see https://arxiv.org/pdf/1706.04129.pdf)
- * 	Specifically, here is how we encode a tree: if it's a terminal,
- * 	encode the index of the terminal in the grammar. 
- * 	If it's a nonterminal, encode the rule with a modular pairing function
- * 	and then use Rosenberg-Strong encoding to encode each of the children. 
- * 	In this way, we can make a 1-1 pairing between trees and integers,
- * 	and therefore store trees as integers in a reversible way
- * 	as well as enumerate by giving this integers. 
-
- * @param g - the grammar
- * @param nt
- * @param is
- * @return 
- */
 template<typename Grammar_t>
-Node expand_from_integer(Grammar_t* g, nonterminal_t nt, IntegerizedStack& is) {
-
-	// NOTE: for now this doesn't work when nt only has finitely many expansions/trees
-	// below it. Otherwise we'll get an assertion error eventually.
+class FullCFGEnumeration : public EnumerationInterface<Grammar_t> {
+public:
+	FullCFGEnumeration(Grammar_t* g) : EnumerationInterface<Grammar_t>(g) {}
 	
-	enumerationidx_t numterm = g->count_terminals(nt);
-	if(is.get_value() < numterm) {
-		return g->makeNode(g->get_rule(nt, is.get_value()));	// whatever terminal we wanted
-	}
-	else {
-		// Otherwise it is encoding numterm+which_terminal
-		is -= numterm; 
-		
-		auto ri = is.pop(g->count_nonterminals(nt)); // this already includes the shift from all the nonterminals
-		
-		Rule* r = g->get_rule(nt, ri+numterm); // shift index from terminals (because they are first!)
-		Node out = g->makeNode(r);
-		for(size_t i=0;i<r->N;i++) {
-			// note that this recurses on the enumerationidx_t format -- so it makes a new IntegerizedStack for each child
-			out.set_child(i, expand_from_integer(g, r->type(i), i==r->N-1 ? is.get_value() : is.pop()) ) ; 
+	/**
+	 * @brief This is a handy function to enumerate trees produced by the grammar. 
+	 *  This works by encoding each tree into an integer using some "pairing functions" 
+	 *  (see https://arxiv.org/pdf/1706.04129.pdf)
+	 * 	Specifically, here is how we encode a tree: if it's a terminal,
+	 * 	encode the index of the terminal in the grammar. 
+	 * 	If it's a nonterminal, encode the rule with a modular pairing function
+	 * 	and then use Rosenberg-Strong encoding to encode each of the children. 
+	 * 	In this way, we can make a 1-1 pairing between trees and integers,
+	 * 	and therefore store trees as integers in a reversible way
+	 * 	as well as enumerate by giving this integers. 
+	 */
+	virtual Node toNode(nonterminal_t nt, IntegerizedStack& is) override {
+	
+		// NOTE: for now this doesn't work when nt only has finitely many expansions/trees
+		// below it. Otherwise we'll get an assertion error eventually.
+		enumerationidx_t numterm = this->grammar->count_terminals(nt);
+		if(is.get_value() < numterm) {
+			return this->grammar->makeNode(this->grammar->get_rule(nt, is.get_value()));	// whatever terminal we wanted
 		}
-		
-		return out;					
-	}
-
-}
-
-template<typename Grammar_t>
-Node expand_from_integer(Grammar_t* g, nonterminal_t nt, enumerationidx_t z) {
-	++FleetStatistics::enumeration_steps;
-	
-	IntegerizedStack is(z);
-	return expand_from_integer(g, nt, is);
-}
-	
-template<typename Grammar_t>
-enumerationidx_t compute_enumeration_order(Grammar_t* g, const Node& n) {
-	// inverse of the above function -- what order would we be enumerated in?
-	if(n.nchildren() == 0) {
-		return g->get_index_of(n.rule);
-	}
-	else {
-		// here we work backwards to encode 
-		nonterminal_t nt = n.rule->nt;
-		enumerationidx_t numterm = g->count_terminals(nt);
-		
-		IntegerizedStack is(compute_enumeration_order(g, n.child(n.rule->N-1)));
-		for(int i=n.rule->N-2;i>=0;i--) {
-			is.push(compute_enumeration_order(g, n.child(i)));
+		else {
+			// Otherwise it is encoding numterm+which_terminal
+			is -= numterm; 
+			
+			auto ri = is.pop(this->grammar->count_nonterminals(nt)); // this already includes the shift from all the nonterminals
+			
+			Rule* r = this->grammar->get_rule(nt, ri+numterm); // shift index from terminals (because they are first!)
+			Node out = this->grammar->makeNode(r);
+			for(size_t i=0;i<r->N;i++) {
+				// note that this recurses on the enumerationidx_t format -- so it makes a new IntegerizedStack for each child
+				out.set_child(i, toNode(this->grammar, r->type(i), i==r->N-1 ? is.get_value() : is.pop()) ) ; 
+			}
+			
+			return out;					
 		}
-		is.push(g->get_index_of(n.rule)-numterm, g->count_nonterminals(nt));
-		is += numterm;
-		return is.get_value();
 	}
-}
+	
+	virtual enumerationidx_t toInteger(const Node& n) override {
+		// inverse of the above function -- what order would we be enumerated in?
+		if(n.nchildren() == 0) {
+			return this->grammar->get_index_of(n.rule);
+		}
+		else {
+			// here we work backwards to encode 
+			nonterminal_t nt = n.rule->nt;
+			enumerationidx_t numterm = this->grammar->count_terminals(nt);
+			
+			IntegerizedStack is(toInteger(this->grammar, n.child(n.rule->N-1)));
+			for(int i=n.rule->N-2;i>=0;i--) {
+				is.push(toInteger(this->grammar, n.child(i)));
+			}
+			is.push(this->grammar->get_index_of(n.rule)-numterm, this->grammar->count_nonterminals(nt));
+			is += numterm;
+			return is.get_value();
+		}
+	}
+};
+
 
 /*
 	Node lempel_ziv_full_expand(nonterminal_t nt, enumerationidx_t z, Node* root=nullptr) const {
@@ -303,188 +356,98 @@ enumerationidx_t compute_enumeration_order(Grammar_t* g, const Node& n) {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-
-//
-//template<typename Grammar_t>
-//[[nodiscard]] Node partial_subtree(Grammar_t* g, const Node& n, IntegerizedStack& is) {
-//	// make a copy of the z'th partial subtree 
-//
-//	if(is.get_value() == 0) {
-//		return Node();
-//	}
-//	else {
-//		is -= 1; // for not null
-//		
-//		Node out = g->makeNode(n.rule); // copy the first level
-//		for(size_t i=0;i<out.nchildren();i++) {
-//			
-//			// the most we can pop from a child is the number of partial trees they have
-//			auto cz = count_partial_subtrees(n.child(i));
-//			//CERR "  =" TAB i TAB is.get_value() TAB cz TAB n.child(i).string() ENDL;
-//			
-//			// Problem is that when we mod pop from everything before, sometimes we are left with 
-//			// too much at the end?
-//			
-//			
-//			// if we are on the last child, we must use everything
-////			if(i == out.nchildren()-1) {
-////				assert(is.get_value() <= cz);
-////				out.set_child(i, partial_subtree(g, n.child(i), is.get_value() ));
-////			}
-////			else {
-////				out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));
-////			}
-//			out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));
-//		
-//		}
-//		return out;
-//		
-//	}
-//}
-
-
 /**
- * @brief How many partial subtrees are there?
- * @param n
- * @return 
+ * @class SubtreeEnumeration
+ * @author piantado
+ * @date 02/01/21
+ * @file Enumeration.h
+ * @brief Enumerate subtrees of a given tree
  */
-
-[[nodiscard]] enumerationidx_t count_partial_subtrees(const Node& n) {
-	// how many possible connected, partial subtrees do I have
-	// (e.g. including a path back to my root)
-			
-	if(n.is_null()) 
-		return 1; // only one tree there!
-
-	enumerationidx_t k=1; 
-	for(const auto& c : n.get_children() ){
-		// for each child independently, I can either make them null, or I can include them
-		// and choose one of their own subtrees
-		k *= count_partial_subtrees(c); 
-	}
-	return k+1; // +1 because I could make the root null
-}
-
-
-
-
-
-/**
- * @brief Get this partial subtree in an enumeration (uses IntegerizedStack instead of z)
- * @param g
- * @param n
- * @param is
- * @return 
- */
-
 template<typename Grammar_t>
-[[nodiscard]] Node partial_subtree(Grammar_t* g, const Node& n, IntegerizedStack& is) {
-	// make a copy of the z'th partial subtree 
+class SubtreeEnumeration : public EnumerationInterface<Grammar_t> {
+	using Super = EnumerationInterface<Grammar_t>;
+public:
+	SubtreeEnumeration(Grammar_t* g) : Super(g) {}
+		
+	/**
+	 * @brief How many partial subtrees are there?
+	 * @param n
+	 * @return 
+	 */
+	[[nodiscard]] enumerationidx_t count(const Node& n) {
+		// how many possible connected, partial subtrees do I have
+		// (e.g. including a path back to my root)
+				
+		if(n.is_null()) 
+			return 1; // only one tree there!
 
-	if(is.get_value() == 0) {
-		return Node();
-	}
-	else {
-		is -= 1; // is encodes tree+1
-		Node out = g->makeNode(n.rule); // copy the first level
-		for(size_t i=0;i<out.nchildren();i++) {
-			auto cz = count_partial_subtrees(n.child(i));
-			out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));		
+		enumerationidx_t k=1; 
+		for(const auto& c : n.get_children() ){
+			// for each child independently, I can either make them null, or I can include them
+			// and choose one of their own subtrees
+			k *= count(c); 
 		}
-		return out;
-		
+		return k+1; // +1 because I could make the root null
 	}
-}
 
-/**
- * @brief Get the z'th partial subtree in an enumeration
- * @param g
- * @param n
- * @param z
- * @return 
- */
-template<typename Grammar_t>
-[[nodiscard]] Node partial_subtree(Grammar_t* g, const Node& n, enumerationidx_t z) {
-	IntegerizedStack is(z);
-	return partial_subtree(g, n, is);
-}
-
-
-/**
- * @brief What order am I in a partial subtree enumeration from x?
- * @param g
- * @param n
- * @param is
- */
-template<typename Grammar_t>
-[[nodiscard]] enumerationidx_t compute_partial_subtree_order(Grammar_t* g, const Node& x, const Node& n) {
-	
-	if(n.is_null()) {
-		return 0;
+	/**
+	 * @brief Convert to the is'th partial subtree of frm
+	 * @param nt
+	 * @param is
+	 * @return 
+	 */
+	[[nodiscard]] virtual Node toNode(nonterminal_t nt, const Node& frm, IntegerizedStack& is) override {
+		if(is.get_value() == 0) {
+			return Node();
+		}
+		else {
+			is -= 1; // is encodes tree+1
+			Node out = this->grammar->makeNode(frm.rule); // copy the first level
+			for(size_t i=0;i<out.nchildren();i++) {
+				auto cz = count(frm.child(i));
+				out.set_child(i, toNode(frm.type(i), frm.child(i), is.pop(cz) ));		
+			}
+			return out;
+		}
 	}
-	else {
-		// else it's a real construction
-		
-		const int N = n.nchildren();
-		assert( (int)x.nchildren() == N);
+
+	// Required here or else we get z converted implicity to IntegerizedStack
+	[[nodiscard]] virtual Node toNode(nonterminal_t nt, const Node& frm, enumerationidx_t z) override {
+		return Super::toNode(nt,frm,z);
+	}
 	
-		IntegerizedStack is;
-		
-		// We have to go in reverse order here, remember, since that's hwo things are unpacked
-		for(int i=N-1;i>=0;i--) {
-		
-			// the most we can pop from a child is the number of partial trees they have
-			auto cx = count_partial_subtrees(x.child(i));
-			auto k = compute_partial_subtree_order(g, x.child(i), n.child(i));
+
+	[[nodiscard]] virtual enumerationidx_t toInteger(const Node& frm, const Node& n) override {
+		if(n.is_null()) {
+			return 0;
+		}
+		else {
+			// else it's a real construction
 			
-			is.push(k, cx);
+			const int N = n.nchildren();
+			assert( (int)frm.nchildren() == N);
+		
+			IntegerizedStack is;
 			
-			//COUT ">>" TAB k TAB cx TAB is.get_value() TAB "[" << i << "] " << n.child(i) << " in " << n.string() ENDL;
+			// We have to go in reverse order here, remember, since that's hwo things are unpacked
+			for(int i=N-1;i>=0;i--) {
+			
+				// the most we can pop from a child is the number of partial trees they have
+				auto cx = count(frm.child(i));
+				auto k = toInteger(frm.child(i), n.child(i));
+				
+				is.push(k, cx);
+				
+				//COUT ">>" TAB k TAB cx TAB is.get_value() TAB "[" << i << "] " << n.child(i) << " in " << n.string() ENDL;
+			}
+			
+			// 1+ becuase it wasn't null
+			return 1+is.get_value();		
 		}
 		
-		// 1+ becuase it wasn't null
-		return 1+is.get_value();		
 	}
-}
 
+};
 
-
-//template<typename Grammar_t>
-//[[nodiscard]] enumerationidx_t compute_partial_subtree_order(Grammar_t* g, const Node& n) {
-//	
-//	if(n.is_null()) {
-//		return 0;
-//	}
-//	else {
-//		
-//		const int N = n.nchildren();
-//	
-//		IntegerizedStack is;
-//		
-//		// push the last (which is not popped mod)
-//		
-//		// We have to go in reverse order here, remember, since that's hwo things are unpacked
-//		//IntegerizedStack is(compute_partial_subtree_order(g, n.child(N-1)));
-//		for(int i=N-1;i>=0;i--) {
-//		//for(enumerationidx_t i=0;i<N;i++) {
-//			
-//			// Problem here is that there are two encodings of null -- 
-//			
-//			
-//			// the most we can pop from a child is the number of partial trees they have
-//			auto cz = count_partial_subtrees(n.child(i));
-//			auto cx = compute_partial_subtree_order(g, n.child(i));
-//
-//			is.push(cx, cz);
-//			
-//			COUT ">>" TAB cx TAB cz TAB is.get_value() TAB n.child(i) << " in " << n.string() ENDL;
-//
-//		}
-//		
-//		
-//		// 1+ becuase it wasn't null
-//		return 1+is.get_value();		
-//	}
-//}
 
  
