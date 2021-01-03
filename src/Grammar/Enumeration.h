a9f961792750a1996280813715598d0a5cda9989
Steve Piantadosi <spiantado@gmail.com>
2021-01-02 22:17:42 -0800

    fixed enumeration for partial trees

--- src/Grammar/Enumeration.h
+++ src/Grammar/Enumeration.h
@@ -1,10 +1,23 @@
 #pragma once
 
-
 #include "FleetStatistics.h"
 #include "IntegerizedStack.h"
 
-/* Functions for enumerating trees */
+/**
+ * @class EnumerationInterface
+ * @author piantado
+ * @date 02/01/21
+ * @file Enumeration.h
+ * @brief This interface an easy enumeratino interface which provides an abstract class for various
+ * 		  types of enumeration. Most need to know about the grammar type.  
+ */
+template<typename Grammar_t>
+class EnumerationInterface {
+
+
+	
+};
+
 
 /**
  * @brief This is a handy function to enumerate trees produced by the grammar. 
@@ -289,6 +302,47 @@ enumerationidx_t compute_enumeration_order(Grammar_t* g, const Node& n) {
 // Subtrees
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+
+//
+//template<typename Grammar_t>
+//[[nodiscard]] Node partial_subtree(Grammar_t* g, const Node& n, IntegerizedStack& is) {
+//	// make a copy of the z'th partial subtree 
+//
+//	if(is.get_value() == 0) {
+//		return Node();
+//	}
+//	else {
+//		is -= 1; // for not null
+//		
+//		Node out = g->makeNode(n.rule); // copy the first level
+//		for(size_t i=0;i<out.nchildren();i++) {
+//			
+//			// the most we can pop from a child is the number of partial trees they have
+//			auto cz = count_partial_subtrees(n.child(i));
+//			//CERR "  =" TAB i TAB is.get_value() TAB cz TAB n.child(i).string() ENDL;
+//			
+//			// Problem is that when we mod pop from everything before, sometimes we are left with 
+//			// too much at the end?
+//			
+//			
+//			// if we are on the last child, we must use everything
+////			if(i == out.nchildren()-1) {
+////				assert(is.get_value() <= cz);
+////				out.set_child(i, partial_subtree(g, n.child(i), is.get_value() ));
+////			}
+////			else {
+////				out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));
+////			}
+//			out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));
+//		
+//		}
+//		return out;
+//		
+//	}
+//}
+
+
 /**
  * @brief How many partial subtrees are there?
  * @param n
@@ -303,15 +357,17 @@ enumerationidx_t compute_enumeration_order(Grammar_t* g, const Node& n) {
 		return 1; // only one tree there!
 
 	enumerationidx_t k=1; 
-	for(enumerationidx_t i=0;i<n.nchildren();i++){
-		if(not n.child(i).is_null())  {
-			// for each child independently, I can either make them null, or I can include them
-			// and choose one of their own subtrees
-			k = k * count_partial_subtrees(n.child(i)); 
-		}
+	for(const auto& c : n.get_children() ){
+		// for each child independently, I can either make them null, or I can include them
+		// and choose one of their own subtrees
+		k *= count_partial_subtrees(c); 
 	}
 	return k+1; // +1 because I could make the root null
 }
+
+
+
+
 
 /**
  * @brief Get this partial subtree in an enumeration (uses IntegerizedStack instead of z)
@@ -329,29 +385,11 @@ template<typename Grammar_t>
 		return Node();
 	}
 	else {
-		is -= 1; // for not null
-		
+		is -= 1; // is encodes tree+1
 		Node out = g->makeNode(n.rule); // copy the first level
 		for(size_t i=0;i<out.nchildren();i++) {
-			
-			// the most we can pop from a child is the number of partial trees they have
 			auto cz = count_partial_subtrees(n.child(i));
-			//CERR "  =" TAB i TAB is.get_value() TAB cz TAB n.child(i).string() ENDL;
-			
-			// Problem is that when we mod pop from everything before, sometimes we are left with 
-			// too much at the end?
-			
-			
-			// if we are on the last child, we must use everything
-//			if(i == out.nchildren()-1) {
-//				assert(is.get_value() <= cz);
-//				out.set_child(i, partial_subtree(g, n.child(i), is.get_value() ));
-//			}
-//			else {
-//				out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));
-//			}
-			out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));
-		
+			out.set_child(i, partial_subtree(g, n.child(i), is.pop(cz) ));		
 		}
 		return out;
 		
@@ -373,44 +411,80 @@ template<typename Grammar_t>
 
 
 /**
- * @brief What order am I in a partial subtree enumeration?
+ * @brief What order am I in a partial subtree enumeration from x?
  * @param g
  * @param n
  * @param is
  */
 template<typename Grammar_t>
-[[nodiscard]] enumerationidx_t compute_partial_subtree_order(Grammar_t* g, const Node& n) {
+[[nodiscard]] enumerationidx_t compute_partial_subtree_order(Grammar_t* g, const Node& x, const Node& n) {
 	
 	if(n.is_null()) {
 		return 0;
 	}
 	else {
+		// else it's a real construction
 		
 		const int N = n.nchildren();
+		assert( (int)x.nchildren() == N);
 	
 		IntegerizedStack is;
 		
-		// push the last (which is not popped mod)
-		
 		// We have to go in reverse order here, remember, since that's hwo things are unpacked
-		//IntegerizedStack is(compute_partial_subtree_order(g, n.child(N-1)));
 		for(int i=N-1;i>=0;i--) {
-		//for(enumerationidx_t i=0;i<N;i++) {
-			
-			// the most we can pop from a child is the number of partial trees they have
-			auto cz = count_partial_subtrees(n.child(i));
-			auto cx = compute_partial_subtree_order(g, n.child(i));
-
-			is.push(cx, cz);
-			
-			COUT ">>" TAB cx TAB cz TAB is.get_value() TAB n.child(i) << " in " << n.string() ENDL;
-
-		}
 		
+			// the most we can pop from a child is the number of partial trees they have
+			auto cx = count_partial_subtrees(x.child(i));
+			auto k = compute_partial_subtree_order(g, x.child(i), n.child(i));
+			
+			is.push(k, cx);
+			
+			//COUT ">>" TAB k TAB cx TAB is.get_value() TAB "[" << i << "] " << n.child(i) << " in " << n.string() ENDL;
+		}
 		
 		// 1+ becuase it wasn't null
 		return 1+is.get_value();		
 	}
 }
+
+
+
+//template<typename Grammar_t>
+//[[nodiscard]] enumerationidx_t compute_partial_subtree_order(Grammar_t* g, const Node& n) {
+//	
+//	if(n.is_null()) {
+//		return 0;
+//	}
+//	else {
+//		
+//		const int N = n.nchildren();
+//	
+//		IntegerizedStack is;
+//		
+//		// push the last (which is not popped mod)
+//		
+//		// We have to go in reverse order here, remember, since that's hwo things are unpacked
+//		//IntegerizedStack is(compute_partial_subtree_order(g, n.child(N-1)));
+//		for(int i=N-1;i>=0;i--) {
+//		//for(enumerationidx_t i=0;i<N;i++) {
+//			
+//			// Problem here is that there are two encodings of null -- 
+//			
+//			
+//			// the most we can pop from a child is the number of partial trees they have
+//			auto cz = count_partial_subtrees(n.child(i));
+//			auto cx = compute_partial_subtree_order(g, n.child(i));
+//
+//			is.push(cx, cz);
+//			
+//			COUT ">>" TAB cx TAB cz TAB is.get_value() TAB n.child(i) << " in " << n.string() ENDL;
+//
+//		}
+//		
+//		
+//		// 1+ becuase it wasn't null
+//		return 1+is.get_value();		
+//	}
+//}
 
  
