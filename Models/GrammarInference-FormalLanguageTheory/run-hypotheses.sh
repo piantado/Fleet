1b4b17dd4dce0175d605585fa02c0987b932ebdb
Steven Piantadosi <spiantado@gmail.com>
2021-07-16 16:25:07 -0700

    done reparameterizing

--- Models/GrammarInference-FormalLanguageTheory/run-hypotheses.sh
+++ Models/GrammarInference-FormalLanguageTheory/run-hypotheses.sh
@@ -1,6 +1,8 @@
 
-./main --inner-time=1m --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/1m-1.txt
+#./main --inner-time=1m --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/1m
 
-./main --inner-time=5m --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/5m-1.txt
+#./main --inner-time=5m --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/5m
 
-./main --inner-time=10m --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/10m-1.txt
+#./main --inner-time=10m --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/10m
+
+./main --inner-time=1h --runtype=hypotheses --alphabet=abcd --hypotheses=hypotheses/1h
